3831d45bf5be88b6c2517a6da348695505afe730
Brenton Hauth <brentonh@rogers.com>
2021-02-22 21:47:31 -0500

    Initial Commit

---
+++ app.js
@@ -0,0 +1,10 @@
+const PORT = process.env.PORT || 5000;
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => res.send('<h2>Index</h2>'));
+app.get('/account', (req, res) => res.send('<h2>Account</h2>'));
+
+app.listen(PORT, () => {
+    console.log(`> Listening on port ${PORT}...`);
+});
