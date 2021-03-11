9b091ddfd64685b73a2e82b95b1aadd1c1d39bd8
Brenton Hauth <brentonh@rogers.com>
2021-03-11 14:45:45 -0500

    Admin update user endpoint

--- src/api/admin.js
+++ src/api/admin.js
@@ -1,6 +1,7 @@
 const express = require('express').Router;
 const auth = require('../middleware/auth');
 const { ok } = require('../helpers/response');
+const User = require('../models/user');
 
 
 const router = express();
@@ -12,8 +13,23 @@ router.get('/user/:id', (req, res) => {
     res.json(ok({}));
 });
 
-router.post('/user/:id', (req, res) => {
-    res.json(ok({}));
+router.post('/user/:id', (req, res, next) => {
+    let _id = req.params.id;
+    User.findOne({ _id }).then(async user => {
+        const body = req.body;
+        for (let i in body) {
+            if (body[i] && user[i] !== undefined) {
+                user[i] = body[i];
+            }
+        }
+        user.save((err, doc) => {
+            if (err) {
+                next(err);
+            } else {
+                res.json(ok(doc, 'Successfuly updated'));
+            }
+        });
+    }).catch(next);
 });
 
 router.get('/user/search', (req, res) => {
