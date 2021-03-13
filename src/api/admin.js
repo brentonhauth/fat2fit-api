ef3416a80f42d69cd81953ca33c1195af01187a5
ThileepanParamanantham <73500446+ThileepanParamanantham@users.noreply.github.com>
2021-03-13 01:46:55 -0500

    Update admin.js
    added all user output for /users route and output specific user for /search by email

--- src/api/admin.js
+++ src/api/admin.js
@@ -1,6 +1,7 @@
 const express = require('express').Router;
 const auth = require('../middleware/auth');
 const { ok } = require('../helpers/response');
+const User = require('../models/user');
 
 
 const router = express();
@@ -16,8 +17,29 @@ router.post('/user/:id', (req, res) => {
     res.json(ok({}));
 });
 
-router.get('/user/search', (req, res) => {
-    res.json(ok({}));
+//returns all user infomation if requested by admin
+//route /admin/users
+router.get('/users', (req,res,next) =>{
+    User.find({},function(err,result){
+        if(err){
+            return next(err);
+        }else{
+            res.json(ok(result));
+        }
+    });
+});
+
+//returns user infomation based on email sent
+//route /admin/search
+router.post('/search', (req, res,next) => {
+    var email = req.body.email;
+    User.find({'email':email}, function(err,result){
+        if(err){
+            return next(err);
+        }else{
+            res.json(ok(result));
+        }
+    });
 });
 
 
