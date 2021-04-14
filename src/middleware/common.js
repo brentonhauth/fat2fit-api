855f2c2d1a474298249b8aace4f7d32d1a68cd71
Brenton Hauth <brentonh@rogers.com>
2021-04-14 18:58:26 -0400

    Cors & morgan

--- src/middleware/common.js
+++ src/middleware/common.js
@@ -1,14 +1,31 @@
 const express = require('express');
 const helmet = require('helmet');
+const cors = require('cors');
+const config = require('../config');
 const { urlencoded, json } = require('body-parser');
+
+/**
+ * @param {express.Application} app
+ */
+function development(app) {
+    const morgan = require('morgan');
+    app.use(morgan('dev'));
+}
 
 /**
  * Appends common middleware to a router or app
  * @param {express.Application} app 
  */
 module.exports = app => {
-    app.use(express.static('./public'));
+    if (/^dev/.test(config.ENV)) {
+        development(app);
+    }
     app.use(helmet());
+    app.use(cors({
+        allowedHeaders: ['Origin', 'Authorization', 'Content-Type'],
+        origin: '*'
+    }));
+    app.use(express.static('./public'));
     app.use(urlencoded({ extended: false }));
     app.use(json());
     return app;
